56616c9bc9e66582013a1fb624cd105d74c92bef
Limaaron <44295291+Limaaron@users.noreply.github.com>
2021-05-02 23:51:31 +0300

    [Add] dvmdef.h file

---
+++ src/runtime/dvmdef.h
@@ -0,0 +1,37 @@
+#ifndef VIRTUALDRAGON_DVMDEF_H
+#define VIRTUALDRAGON_DVMDEF_H
+
+#define REGISTER_COUNT 32
+
+#if REGISTER_COUNT < 8
+#   error REGISTER_COUNT Minimum allowed value in REGISTER_COUNT 8
+#endif
+
+#define nIP  31     // n instruction pointer
+#define nSP  30     // n stack pointer
+#define nBP  29     // n base pointer
+#define nTP  28     // n this pointer
+#define nRA  27     // n return address
+
+#define IP  state->rn[ nIP ]    // instruction pointer
+#define SP  state->rn[ nSP ]    // stack pointer
+#define BP  state->rn[ nBP ]    // base pointer
+#define TP  state->rn[ nTP ]    // this pointer
+#define RA  state->rn[ nRA ]    // return address
+
+#define PUSH( type, var )                   \
+    ( (type*)state->stack )[ SP ] = var;    \
+    SP += sizeof(type)
+
+#define POP( type )             \
+    SP -= sizeof(type);         \
+    ((type*)state->stack)[ SP ]
+
+#define vmdispatch(o)	switch(o)
+#define vmswitch(o)     switch(o)
+#define vmcase(l)	    case l:
+#define vmbreak		    break
+#define vmdefault       default:
+#define vmrelease       return
+
+#endif // VIRTUALDRAGON_DVMDEF_H
