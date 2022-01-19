336dab0a4f0f6da2272bf9fab26bce01071747ff
Limaaron <limaaron@vk.com>
2022-01-19 03:19:42 +0300

    [Delete] Automatically generated text

--- src/libdvm/dvm-32_64/format-j/format-j.h
+++ src/libdvm/dvm-32_64/format-j/format-j.h
@@ -1,8 +1,12 @@
-//
-// Created by Limaaron on 20.12.2021.
-//
-
 #ifndef VIRTUALDRAGON_FORMAT_J_H
 #define VIRTUALDRAGON_FORMAT_J_H
+
+#define DVM_FORMAT_I (3)
+
+#include "virtualdragon.h"
+#include "dvmclass.h"
+
 // jump 24 bit imm
+VOID DVM_FASTCALL format_j(DVM* state, duint32 instruction);
+
 #endif // VIRTUALDRAGON_FORMAT_J_H
