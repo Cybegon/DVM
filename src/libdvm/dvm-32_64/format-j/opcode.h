cd9b5ddd56ed1eef2a4b8f8c6267bc0567719661
Limaaron <limaaron@vk.com>
2022-01-19 04:32:18 +0300

    [Add] format-j opcode.h

---
+++ src/libdvm/dvm-32_64/format-j/opcode.h
@@ -0,0 +1,11 @@
+#ifndef VIRTUALDRAGON_OPCODE_H
+#define VIRTUALDRAGON_OPCODE_H
+
+#define OP_JMP  (0x00)
+#define OP_JE   (0x01)
+#define OP_JNE  (0x02)
+#define OP_JL   (0x03)
+#define OP_JGE  (0x04)
+//#define OP_
+
+#endif //VIRTUALDRAGON_OPCODE_H
