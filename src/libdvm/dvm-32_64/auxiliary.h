c38ff4511c885741c9b97a653b9008c405e64444
Limaaron <limaaron@vk.com>
2022-02-15 02:35:38 +0300

    [Fix] branching instructions

--- src/libdvm/dvm-32_64/auxiliary.h
+++ src/libdvm/dvm-32_64/auxiliary.h
@@ -4,17 +4,17 @@
 #define IF_EQ() if ( (FR & EQ) )        // EQual
 #define IF_NE() if (!(FR & EQ) )        // Not Equal
 
-// Unsigned
-#define IF_LO() if ( (FR & LO) )      // LOwer
-#define IF_HI() if (!(FR & LO) )      // HIgher
-#define IF_LS() if ( (FR & (LO|EQ)) ) // Lower or Same
-#define IF_HS() if (!(FR & (LO|EQ)) ) // Higher or Same
-
 // Signed
-#define IF_LT() if ( (FR & LT) )        // Less Than
-#define IF_GT() if (!(FR & LT) )        // Greater Than
 #define IF_LE() if ( (FR & (LT|EQ)) )   // Less than or Equal
-#define IF_GE() if (!(FR & (LT|EQ)) )   // Greater than or Equal
+#define IF_GE() if (!(FR & LT) )        // Greater than or Equal
+#define IF_LT() if ( (FR & LT) )        // Less Than
+#define IF_GT() if (!(FR & (LT|EQ)) )   // Greater Than
+
+// Unsigned
+#define IF_LS() if ( (FR & (LO|EQ)) )   // Lower or Same
+#define IF_HS() if (!(FR & LO) )        // Higher or Same
+#define IF_LO() if ( (FR & LO) )        // LOwer
+#define IF_HI() if (!(FR & (LO|EQ)) )   // HIgher
 
 #if defined(DVM_ENABLE_JUMP)
 #   define JUMP() IP = DVM_GET_IMM24(instruction)
