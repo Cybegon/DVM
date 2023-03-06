5a57918dcc872ad8322536c11f4eb60e635d2280
Tim K <timk@xfen.page>
2023-03-06 18:38:30 +0100

    fixed header guards being the same for i64

--- src/libcar/i64/format-c/format-c.h
+++ src/libcar/i64/format-c/format-c.h
@@ -1,9 +1,9 @@
-#ifndef VIRTUALDRAGON_FORMAT_C_H
-#define VIRTUALDRAGON_FORMAT_C_H
+#ifndef VIRTUALDRAGON_FORMAT64_C_H
+#define VIRTUALDRAGON_FORMAT64_C_H
 
 #include "dvm.h"
 
 // jump 48 bit imm
 VOID DVM_FASTCALL format_c64(DVM* state, duint64 instruction);
 
-#endif //VIRTUALDRAGON_FORMAT_C_H
+#endif //VIRTUALDRAGON_FORMAT64_C_H
--- src/libcar/i64/format-j/format-j.h
+++ src/libcar/i64/format-j/format-j.h
@@ -1,9 +1,9 @@
-#ifndef VIRTUALDRAGON_FORMAT_J_H
-#define VIRTUALDRAGON_FORMAT_J_H
+#ifndef VIRTUALDRAGON_FORMAT64_J_H
+#define VIRTUALDRAGON_FORMAT64_J_H
 
 #include "dvm.h"
 
 // jump 48 bit imm
 VOID DVM_FASTCALL format_j64(DVM* state, duint64 instruction);
 
-#endif //VIRTUALDRAGON_FORMAT_J_H
+#endif //VIRTUALDRAGON_FORMAT64_J_H
