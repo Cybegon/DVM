a7e4a04807c6d669396aecf08ddf3b69567ca798
Limaaron <limaaron@vk.com>
2023-04-09 04:07:24 +0300

    [Add] car_op32Pack64

--- src/public/libcar-gen/car-gen.h
+++ src/public/libcar-gen/car-gen.h
@@ -5,6 +5,8 @@
 
 #define NO_REG 0xFF
 
+duint64 car_op32Pack64(duint32 opFirst, duint32 opSecond);
+
 duint32 car_emit_op32(duint8 format, duint8 opcode,
                       duint8 regDst, duint8 regSrc1,
                       duint8 regSrc2, duint8 regSrc3,
