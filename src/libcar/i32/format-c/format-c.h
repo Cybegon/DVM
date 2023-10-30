8684cd283904ec747549b7a0e9e12327b4d73769
Limaaron <limaaron@vk.com>
2023-10-31 00:56:14 +0300

    [Delete] 32 bit flow formats

--- src/libcar/i32/format-c/format-c.h
+++
@@ -1,9 +0,0 @@
-#ifndef VIRTUALDRAGON_FORMAT_C_H
-#define VIRTUALDRAGON_FORMAT_C_H
-
-#include "dvm.h"
-
-// jump 24 bit imm
-VOID DVM_FASTCALL format_c32(DVM* state, duint32 instruction);
-
-#endif //VIRTUALDRAGON_FORMAT_C_H
--- src/libcar/i32/format-j/format-j.h
+++
@@ -1,9 +0,0 @@
-#ifndef VIRTUALDRAGON_FORMAT_J_H
-#define VIRTUALDRAGON_FORMAT_J_H
-
-#include "dvm.h"
-
-// jump 24 bit imm
-VOID DVM_FASTCALL format_j32(DVM* state, duint32 instruction);
-
-#endif // VIRTUALDRAGON_FORMAT_J_H
